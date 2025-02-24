7435cf1c607ad49607a789379e53907001806953
Bambi <milan.t.kadri@gmail.com>
2025-02-24 15:01:41 +0100

    changed compileSdk + targetSdk to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,12 +11,12 @@ plugins {
 
 android {
     namespace = "com.example.weathermonkey"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.weathermonkey"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
