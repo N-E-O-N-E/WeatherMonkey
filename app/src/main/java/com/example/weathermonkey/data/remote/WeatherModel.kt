d7d96caeda6d21f6be93a45b37549a27bd8bb2af
Kai Becker <KBecker92@web.de>
2025-02-26 10:13:54 +0100

    DataClass erweitert um Hourly+HourlyUnits

--- app/src/main/java/com/example/weathermonkey/data/remote/WeatherModel.kt
+++ app/src/main/java/com/example/weathermonkey/data/remote/WeatherModel.kt
@@ -20,7 +20,61 @@ data class WeatherModel(
     @Json(name = "daily_units")
     val dailyUnits: DailyUnits,
     @Json(name = "daily")
-    val daily: Daily
+    val daily: Daily,
+    @Json(name = "hourly_units")
+    val hourlyUnits: HourlyUnits,
+    @Json(name = "hourly")
+    val hourly: Hourly
+)
+
+data class HourlyUnits(
+    @Json(name = "time")
+    val time: String,
+    @Json(name = "temperature_2m")
+    val temperature2m: String,
+    @Json(name = "apparent_temperature")
+    val apparentTemperature: String,
+    @Json(name = "precipitation_probability")
+    val precipitationProbability: String,
+    @Json(name = "rain")
+    val rain: String,
+    @Json(name = "showers")
+    val showers: String,
+    @Json(name = "snowfall")
+    val snowfall: String,
+    @Json(name = "weather_code")
+    val weatherCode: String,
+    @Json(name = "cloud_cover_low")
+    val cloudCoverLow: String,
+    @Json(name = "cloud_cover_mid")
+    val cloudCoverMid: String,
+    @Json(name = "cloud_cover_high")
+    val cloudCoverHigh: String
+)
+
+data class Hourly(
+    @Json(name = "time")
+    val time: List<String>,
+    @Json(name = "temperature_2m")
+    val temperature2m: List<Double>,
+    @Json(name = "apparent_temperature")
+    val apparentTemperature: List<Double>,
+    @Json(name = "precipitation_probability")
+    val precipitationProbability: List<Int>,
+    @Json(name = "rain")
+    val rain: List<Double>,
+    @Json(name = "showers")
+    val showers: List<Double>,
+    @Json(name = "snowfall")
+    val snowfall: List<Double>,
+    @Json(name = "weather_code")
+    val weatherCode: List<Int>,
+    @Json(name = "cloud_cover_low")
+    val cloudCoverLow: List<Int>,
+    @Json(name = "cloud_cover_mid")
+    val cloudCoverMid: List<Int>,
+    @Json(name = "cloud_cover_high")
+    val cloudCoverHigh: List<Int>
 )
 
 data class DailyUnits(
