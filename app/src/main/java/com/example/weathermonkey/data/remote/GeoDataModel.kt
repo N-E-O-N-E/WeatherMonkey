4c499a09b6892e5a2e9c75367e67c50841dbc86b
Kai Becker <KBecker92@web.de>
2025-02-26 10:17:29 +0100

    DataClass erweitert um GeoCodeModel

---
+++ app/src/main/java/com/example/weathermonkey/data/remote/GeoDataModel.kt
@@ -0,0 +1,20 @@
+package com.example.weathermonkey.data.remote
+
+data class GeoCodeResponse(
+    val results: List<GeoCodeData>
+)
+
+data class GeoCodeData(
+    val id: Int,
+    val name: String,
+    val latitude: Double,
+    val longitude: Double,
+    val elevation: Double = 0.0,
+    val feature_code: String = "",
+    val country_code: String = "",
+    val timezone: String = "",
+    val population: Int = 0,
+    val country_id: Int = 0,
+    val country: String = "",
+    val postcodes: List<String> = emptyList()
+)
