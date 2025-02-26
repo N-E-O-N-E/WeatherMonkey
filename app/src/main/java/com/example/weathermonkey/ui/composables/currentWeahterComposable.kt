136dbe9b1b6a239a02e8bd0c736fa2a9c2a0ed64
Markus Wirtz <info@markuswirtz.net>
2025-02-26 15:56:23 +0100

    Weather Main Composable

--- app/src/main/java/com/example/weathermonkey/ui/composables/currentWeahterComposable.kt
+++ app/src/main/java/com/example/weathermonkey/ui/composables/currentWeahterComposable.kt
@@ -2,31 +2,77 @@ package com.example.weathermonkey.ui.composables
 
 import WeatherModel
 import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.weathermonkey.R
 import com.example.weathermonkey.data.repository.mockData.mockResponse
-
+import com.example.weathermonkey.utils.currentHourAsString
 
 @Composable
 fun CurrentWeatherComposable(
     modifier: Modifier = Modifier,
     data: WeatherModel
 ) {
+    Surface(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(200.dp)
+            .padding(horizontal = 10.dp),
+        color = Color.White.copy(alpha = 0.4f),
+        shape = RoundedCornerShape(10.dp)
+    ) {
 
-    Surface(modifier = modifier, color = Color.Cyan) {
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(10.dp),
+            horizontalArrangement = Arrangement.SpaceAround,
 
+            ) {
+            Image(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .padding(10.dp),
+                painter = painterResource(id = R.drawable.sunicon),
+                contentDescription = "",
+                contentScale = ContentScale.FillHeight,
+            )
 
+            Text(text = currentHourAsString.getCurrent())
 
+            Text(
+                text = "${data.hourly.temperature2m}°",
+                color = Color.White,
+                fontSize = 100.sp
+            )
+        }
     }
-
-
 }
 
-@Preview(name = "CurrentWeatherComp", device = "id:pixel_7_pro", showSystemUi = true, backgroundColor = 0xFF7BC1CC,
-    uiMode = Configuration.UI_MODE_NIGHT_NO or Configuration.UI_MODE_TYPE_NORMAL
+@Preview(
+    name = "CurrentWeatherComp",
+    device = "id:pixel_7_pro",
+    showSystemUi = false,
+    backgroundColor = 0xFF1BC5F1,
+    uiMode = Configuration.UI_MODE_NIGHT_NO or Configuration.UI_MODE_TYPE_NORMAL,
+    showBackground = true
 )
 @Composable
 private fun CurrentWeatherComposablePreview() {
