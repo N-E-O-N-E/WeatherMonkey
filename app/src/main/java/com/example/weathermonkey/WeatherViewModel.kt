e33cb99f10c8f19bf34d926786b0307851791f27
Bambi <milan.t.kadri@gmail.com>
2025-02-26 09:14:52 +0100

    switched from mutablelivedata to mutablestateflow

--- app/src/main/java/com/example/weathermonkey/WeatherViewModel.kt
+++ app/src/main/java/com/example/weathermonkey/WeatherViewModel.kt
@@ -4,41 +4,31 @@ import android.annotation.SuppressLint
 import android.app.Application
 import android.location.Location
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import com.example.weathermonkey.data.repository.WeatherRepositoryInterface
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.Priority
 import com.google.android.gms.tasks.CancellationTokenSource
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class WeatherViewModel(
     application: Application,
     private val weatherRepository: WeatherRepositoryInterface,
 ) : AndroidViewModel(application) {
 
-
     private val fusedLocationProviderClient: FusedLocationProviderClient =
         LocationServices.getFusedLocationProviderClient(application)
 
-    private val _location = MutableLiveData<Location?>()
-    val location: LiveData<Location?> = _location
+    private val _location = MutableStateFlow<Location?>(null)
+    val location: StateFlow<Location?> = _location.asStateFlow()
 
-    private val _temperature = MutableLiveData<String?>()
-    var temperature = _temperature
-
-//    init {
-//        if (Manifest.permission.ACCESS_FINE_LOCATION == "isGranted" || Manifest.permission.ACCESS_COARSE_LOCATION == "isGranted") { //TODO: make sure this works if permissions have not been set yet
-//            fetchLocation()
-//            viewModelScope.launch {
-//                _temperature.value = listOf(getCurrentWeather(1))
-//            }
-//        }
-//    }
+    private val _temperature = MutableStateFlow<String?>(null)
+    val temperature: StateFlow<String?> = _temperature.asStateFlow()
 
     suspend fun fetchTemperatureByLocation(latitude: Double, longitude: Double) {
         try {
-
             val weatherData = weatherRepository.fetchCurrentWeatherData(
                 latitude = latitude,
                 longitude = longitude,
@@ -49,19 +39,16 @@ class WeatherViewModel(
             val temperatureMin = weatherData.daily.temperature2mMin.firstOrNull()
 
             if (temperatureMax != null && temperatureMin != null) {
-                _temperature.postValue("Max: $temperatureMax°C, Min: $temperatureMin°C")
+                _temperature.value = "Max: $temperatureMax°C, Min: $temperatureMin°C"
             } else {
-                _temperature.postValue("Keine Temperaturdaten verfügbar")
+                _temperature.value = "Keine Temperaturdaten verfügbar"
                 println("Temperature data not available in API response")
             }
-
         } catch (e: Exception) {
-            _temperature.postValue("Fehler: ${e.message}")
+            _temperature.value = "Fehler: ${e.message}"
             println("Fehler beim Abrufen der Wetterdaten: ${e.message}")
         }
     }
-
-
 
     @SuppressLint("MissingPermission")
     fun fetchLocation() {
@@ -70,14 +57,9 @@ class WeatherViewModel(
             Priority.PRIORITY_HIGH_ACCURACY,
             cancellationTokenSource.token
         ).addOnSuccessListener { location ->
-            if (location != null) {
-                _location.postValue(location)
-            } else {
-                _location.postValue(null) //TODO: If else kürzen
-            }
+            _location.value = location
         }.addOnFailureListener {
-            _location.postValue(null)
+            _location.value = null
         }
     }
-
 }
